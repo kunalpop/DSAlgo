8f3bb4da75d66836af9d6737e51ed5d1e75e5301
Kunal Soni <kunalpop@gmail.com>
2022-10-17 20:35:06 +0530

    Update singlyLinkedList.py

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -8,80 +8,60 @@ Created on Sun May 29 15:49:34 2022
 class Node:
     def __init__(self,value=None):
         self.value = value
-        self.prev = None
         self.next = None
 
-class DLinkedList():
+class SLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail= None
+        self.headNode = None
         
+       
     def create_ll(self,arr):
-        self.head = Node(arr[0])
-        
-        prev = self.head
+        self.headNode = Node(arr[0])
+        prev = self.headNode
                 
-        for i in range(1,len(arr)): 
+        for i in range(1,len(arr)):
             
             node = Node(arr[i])
-            prev.next = node
-            node.prev = prev
+            curr = node
+            prev.next = curr
+            prev = curr
             
-            prev = node
-            
-        self.tail = prev
-            
-            
+        return(self.headNode)
+    
     def print_ll(self):
         
-        node = self.head
+        node = self.headNode
         
         ll = []
         
         while(node != None):
             ll.append(node.value)
             node = node.next
-            
-        print(ll)
-
-    def print_reverse_ll(self):
-        
-        node = self.tail
-            
-        ll = []
-        
-        while(node != None):
-            ll.append(node.value)
-            node = node.prev
         
         print(ll)
-        
-        
+                              
     def insert_ll(self,position,value):
         
-        node = self.head
+        node = self.headNode
         
         i=1
-        while(i<=position):
-            
+        while(i<=position):            
             if(i==position):
                 newNode = Node(value)
                 prev.next = newNode
                 newNode.next = node
-                node.prev = newNode
-                newNode.prev = prev
                 i += 1
             else:
                 i += 1
                 prev = node
-                node = node.next
-
+                node = node.next   
+                
     def delete_ll(self,position):
         
-        node = self.head
+        node = self.headNode
         
         i=1
-        while(i<=position):
+        while(i<=position):            
             if(i==position):
                 prev.next = node.next
                 node = None
@@ -89,11 +69,12 @@ class DLinkedList():
             else:
                 i += 1
                 prev = node
-                node = node.next 
+                node = node.next
+
                 
     def update_ll(self,position,value):
         
-        node = self.head
+        node = self.headNode
         
         i=1
         while(i<=position):
@@ -104,56 +85,59 @@ class DLinkedList():
                 i += 1
                 prev = node
                 node = node.next
-            
+                
     def reverse_ll(self):
         
-        node = self.head
+        node = self.headNode
         prev = None
         
         while(node != None):
-            succ = node.next
-            
+
+            suc = node.next
             node.next = prev
-            node.prev = succ
-            
+                        
             prev = node
-            node = succ
+            node = suc
+                
+        self.headNode = prev
         
-        self.tail = self.head
-        self.head = prev
         return()
         
-
 def main():
     
     arr = [16,13,7]
     
-    dll = DLinkedList()
+    sll = SLinkedList()
     
-    dll.create_ll(arr)
-    print("Print a doubly linked list:")
-    dll.print_ll()
+    #Create singly linked list
+    sll.create_ll(arr)
     
-    dll.insert_ll(2,1)
-    print("\nInsert into and print doubly linked list")
-    dll.print_ll()
+    #Print singly linked list
+    print("Print a singly linked list:")
+    sll.print_ll()
     
-    dll.delete_ll(2)
-    print("\nDelete from and print doubly linked list")
-    dll.print_ll()
+    #Insert singly linkedin list
+    sll.insert_ll(2,1)
+    print("\nInsert into and print singly linked list")
+    sll.print_ll()
     
-    dll.update_ll(3,17)
-    print("\nUpdate and print doubly linked list")
-    dll.print_ll()
+    #Delete singly linked list
+    sll.delete_ll(2)
+    print("\nDelete from and print singly linked list")
+    sll.print_ll()
     
-    dll.reverse_ll()
-    print("\nReverse doubly linked list")
-    dll.print_ll()
+    #Update singly linked list
+    sll.update_ll(3,17)
+    print("\nUpdate and print singly linked list")
+    sll.print_ll()
     
+    #Reverse a singly linked list   
+    sll.reverse_ll()
+    print("\nReverse singly linked list")
+    sll.print_ll()
     
 if __name__ == "__main__":
     main()
-    
 
 
     
